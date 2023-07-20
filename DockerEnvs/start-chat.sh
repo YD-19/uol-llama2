9186a84451ce0d591b8bff68e39b9104be517d13
Yi Dong <yi.dong@liverpool.ac.uk>
2023-07-20 22:37:41 +0100

    test

--- DockerEnvs/start-chat.sh
+++ DockerEnvs/start-chat.sh
@@ -7,9 +7,9 @@ chmod 777 chat_test.py
 if [ ${MODEL} = 7b-chat ]; then
     torchrun --nproc_per_node 1 chat_test.py --ckpt_dir llama-2-7b-chat/ --tokenizer_path tokenizer.model --max_seq_len ${LEN} --max_batch_size ${BAT}
 elif [ ${MODEL} = 13b-chat ]; then
-	torchrun --nproc_per_node 2 chat_test.py --ckpt_dir llama-2-13b-chat/ --tokenizer_path tokenizer.model --max_seq_len 2048 --max_batch_size 4
+	torchrun --nproc_per_node 2 chat_test.py --ckpt_dir llama-2-13b-chat/ --tokenizer_path tokenizer.model --max_seq_len ${LEN} --max_batch_size ${BAT}
 elif [ ${MODEL} = 30b-chat ]; then
-	torchrun --nproc_per_node 8 chat_test.py --ckpt_dir llama-2-30b-chat/ --tokenizer_path tokenizer.model --max_seq_len 2048 --max_batch_size 4
+	torchrun --nproc_per_node 8 chat_test.py --ckpt_dir llama-2-30b-chat/ --tokenizer_path tokenizer.model --max_seq_len ${LEN} --max_batch_size ${BAT}
 else
     echo "Must specify a model type in [7b-chat;13b-chat;30b-chat]"
 fi
